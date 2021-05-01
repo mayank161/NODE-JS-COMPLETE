23c8da95fd4eb59c4fcbb6d1a6e021a4463cd9c1
Mayank <satimayank74@gmail.com>
2021-05-01 06:34:15 -0700

    two qution assingment

--- 3.Express Js Working in Node/1. YAV ASSSINGMENT/yavAssingment.js
+++ 3.Express Js Working in Node/1. YAV ASSSINGMENT/yavAssingment.js
@@ -1,17 +1,16 @@
 // Answer 1 => with req we can perform methods of request and for res we can
 // handel request but for next from which we can move down to the next middleware 
 
-// Answer 2 =>
+// Answer 2
 
-const http = require('http');
+const path = require('path');
 const express = require('express');
 
-const radius= express();
+const area = express();
 
-console.log('hi')
-radius.post((req,res,next) => {
-    res.send('<form action="/All-product" method="POST"> <input type="text" name="title"> <button type="submit"> add </button> </form>');
-    //res.sendFile(path.join(__dirname,'radius.html'));
+area.get('/',(req,res,next)=> {
+    console.log('hii');
+    res.sendFile(path.join(__dirname,'radius.html'));
 })
 
-radius.listen(3600);
+area.listen(3600);
